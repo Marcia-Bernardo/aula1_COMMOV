8887199c7dc28efbf8f341e25efa142a3057f820
Márcia Regina Bernardo <79754243+Marcia-Bernardo@users.noreply.github.com>
2022-03-04 16:52:37 +0000

    aula2

--- app/src/main/java/com/example/aula1_commov/MainActivity.kt
+++ app/src/main/java/com/example/aula1_commov/MainActivity.kt
@@ -6,7 +6,34 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        println("Life cycle: onCreate")
+    }
 
-        //fiz alguma coisa
+    override fun onStart() {
+        super.onStart()
+        println("Life cycle: onStart")
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        println("Life cycle: onRestart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("Life cycle: onPause")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("Life cycle: onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("Life cycle: onDestroy")
     }
 }
